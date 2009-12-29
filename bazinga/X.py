1c9abe8a26a86f6644d2161d3ba54e81ce88ce12
Julien Danjou <julien@danjou.info>
2009-12-29 18:51:51 +0100

    fix X
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- bazinga/X.py
+++ bazinga/X.py
@@ -1,10 +1,12 @@
 import pyev
 from MainLoop import MainLoop
+import xcb, xcb.xproto
 
 class X(pyev.Io):
-    def __init__(self, display=None):
-        self.connection = xcb.connect(disaply)
-        pyev.Io.__init__(self, self.connection.get_file_descriptor(), MainLoop(), on_io, self)
+
+    def __init__(self, *args, **kw):
+        connection = xcb.connect(*args, **kw)
+        pyev.Io.__init__(self, connection.get_file_descriptor(), MainLoop(), on_io, self)
 
     def on_io(watcher, events):
         #watcher.data.poll_for_event()
