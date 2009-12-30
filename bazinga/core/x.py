9bcafec7c1e5f30dab4ea48b63930c309d3d3aa7
Julien Danjou <julien@danjou.info>
2009-12-30 11:47:09 +0100

    fix X
    Signed-off-by: Julien Danjou <julien@danjou.info>

---
+++ bazinga/core/x.py
@@ -0,0 +1,17 @@
+import pyev
+from mainloop import MainLoop
+from bazinga.basic import Singleton
+import xcb, xcb.xproto
+
+class X(Singleton, pyev.Io):
+
+    def __init__(self, *args, **kw):
+        if not hasattr(self, "connection"):
+            self.connection = xcb.connect(*args, **kw)
+            pyev.Io.__init__(self, self.connection.get_file_descriptor(),
+                    pyev.EV_READ, MainLoop(), X.on_io, self)
+
+    @staticmethod
+    def on_io(watcher, events):
+        event = watcher.data.poll_for_event()
+        print event
