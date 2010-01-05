f9e0588b7ff81dd270c62062959dc13e35b98942
Julien Danjou <julien@danjou.info>
2010-01-05 16:21:15 +0100

    doc
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- bazinga/core/mainloop.py
+++ bazinga/core/mainloop.py
@@ -3,6 +3,8 @@ from bazinga.basic import Singleton
 
 class MainLoop(Singleton, pyev.Loop):
 
+    """The main bazinga loop."""
+
     def __init__(self):
         Singleton.__init__(self)
         pyev.Loop.__init__(pyev.EVFLAG_NOSIGFD)
