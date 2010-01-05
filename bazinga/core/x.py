cb1a4417ff96426fc25a404fc899726eabb3f5d8
Julien Danjou <julien@danjou.info>
2010-01-05 13:15:10 +0100

    prepare requests
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- bazinga/core/x.py
+++ bazinga/core/x.py
@@ -25,22 +25,22 @@ class Connection(Singleton, pyev.Io):
             # Check that RandR extension is at least 1.1
             randr_queryversion_c = self.connection.randr.QueryVersion(1, 1)
 
-            self.root = []
+            self.roots = []
             self.screens = []
             for root in self.connection.get_setup().roots:
-                self.root.append(Window(id=root.root))
+                self.roots.append(Window(id=root.root))
 
             have_randr = randr_queryversion_c.reply();
 
-            for root in self.root:
-                # Does it have RandR ?
-                if have_randr:
-                    screen_resources = self.connection.randr.GetScreenResources(root.id).reply()
+            # Does it have RandR ?
+            if have_randr:
+                screen_resources_c = Connection.prepare_requests(self.connection.randr.GetScreenResources,
+                        list(root.id for root in self.roots), 0)
+                for screen_resources_cookie in screen_resources_c:
+                    screen_resources = screen_resources_cookie.reply()
 
-                    crtc_info_c = []
-                    for crtc in screen_resources.crtcs:
-                        crtc_info_c.append(self.connection.randr.GetCrtcInfo(crtc,
-                                xcb.xproto.Time.CurrentTime))
+                    crtc_info_c = Connection.prepare_requests(self.connection.randr.GetCrtcInfo,
+                            screen_resources.crtcs, 0, xcb.xproto.Time.CurrentTime)
 
                     for crtc_info_cookie in crtc_info_c:
                         crtc_info = crtc_info_cookie.reply()
@@ -52,20 +52,38 @@ class Connection(Singleton, pyev.Io):
                                     height=crtc_info.height,
                                     outputs=[])
                             self.screens.append(screen)
-                            for output in crtc_info.outputs:
-                                output_info = self.connection.randr.GetOutputInfo(output,
-                                        xcb.xproto.Time.CurrentTime).reply()
+
+                            # Prepare output info requests
+                            output_info_c = Connection.prepare_requests(self.connection.randr.GetOutputInfo,
+                                    crtc_info.outputs, 0, xcb.xproto.Time.CurrentTime)
+
+                            for output_info_cookie in output_info_c:
+                                output_info = output_info_cookie.reply()
                                 screen.outputs.append(Output(name=byte_list_to_str(output_info.name),
                                         mm_width=output_info.mm_width,
                                         mm_height=output_info.mm_height))
 
-                elif have_xinerama:
-                    pass
-                else:
-                    pass
+            elif have_xinerama:
+                pass
+            else:
+                pass
                 
             pyev.Io.__init__(self, self.connection.get_file_descriptor(),
                     pyev.EV_READ, MainLoop(), Connection.on_io)
+
+    @staticmethod
+    def prepare_requests(method, variable_list, position, *args):
+
+        """Prepare a bunch of requests."""
+
+        cookies = []
+
+        for var in variable_list:
+            arguments = list(args)
+            arguments.insert(position, var)
+            cookies.append(method(*arguments))
+
+        return cookies
 
     @staticmethod
     def on_io(watcher, events):
