dc2081294fe0a03880f53452e709612a6ad5df0a
Julien Danjou <julien@danjou.info>
2010-01-12 14:18:11 +0100

    timer: doc
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- bazinga/timer.py
+++ bazinga/timer.py
@@ -3,15 +3,28 @@ import signal
 import basic
 from loop import MainLoop
 
+
 class Timeout(signal.Signal):
+
+    """Timeout signal.  This is send by the timeout object."""
+
     pass
+
 
 class Timer(basic.Object, pyev.Timer):
 
+    """Timer object.
+    This objects send a Timeout signal every time you configure it for."""
+
     def __init__(self, after, repeat, loop=MainLoop()):
+
+        """Initialize a timeout object."""
+
         basic.Object.__init__(self)
         pyev.Timer.__init__(self, after, repeat, loop, Timer.on_timeout)
 
+
     @staticmethod
     def on_timeout(watcher, event):
+
         watcher.emit_signal(signal=Timeout)
