395af27b7398c676d91222673bb9934718a2495f
Julien Danjou <julien@danjou.info>
2010-01-13 11:24:59 +0100

    add decorator

--- bazinga/signal.py
+++ bazinga/signal.py
@@ -1,4 +1,5 @@
 from louie import *
+from decorator import decorator
 
 def emit(signal=signal.All, sender=sender.Anonymous, *arguments, **named):
 
@@ -9,3 +10,16 @@ def emit(signal=signal.All, sender=sender.Anonymous, *arguments, **named):
     for s in [ sender ] + list(sender.__class__.__mro__):
         ret += send(signal, s, *arguments, **named)
     return ret
+
+
+def connected(signal=signal.All, sender=sender.Any, weak=True):
+
+    """Return a decorator function that will connect
+    the underlying function to an event."""
+
+    @decorator
+    def connect_func(func):
+        connect(func, signal, sender)
+        return func
+
+    return connect_func
