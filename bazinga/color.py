953ada0c1300e208d2a7344c97ca1f137dc574d2
Julien Danjou <julien@danjou.info>
2010-01-19 12:35:02 +0100

    color inheritance it's beautiful
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- bazinga/color.py
+++ bazinga/color.py
@@ -5,38 +5,9 @@ from basic import Property
 
 class Color(XObject):
 
-    """A color."""
-
-
-    def __init__(self, colormap, name=None, red=0, blue=0, green=0, alpha=0, **kw):
+    def __init__(self, **kw):
 
         XObject.__init__(self, **kw)
-
-        if name is None or name[0] == '#':
-            if name is not None:
-                len_name = len(name)
-                if len_name != 7 or len_name != 9:
-                    raise ValueError("Bad color name {0}.".format(name))
-
-                red = int(name[1:3], 16) * 257
-                green = int(name[3:5], 16) * 257
-                blue = int(name[5:7], 16) * 257
-
-                if len_name == 9:
-                    alpha = int(name[7:9], 16) * 257
-
-            for value in [ red, blue, green, alpha ]:
-                if value < 0 or value >= 65535:
-                    raise ValueError("Color attribute value is too high.")
-
-            self.cookie = self.connection.core.AllocColor(colormap, red, green, blue)
-        else:
-            if alpha < 0 or alpha >= 65535:
-                raise ValueError("Color attribute value is too high.")
-            self.cookie = self.connection.core.AllocNamedColor(colormap, len(name), name)
-            self._name = name
-
-        self.alpha = alpha
 
 
     def read_reply(self):
@@ -45,14 +16,7 @@ class Color(XObject):
             reply = self.cookie.reply()
             del self.cookie
             self._pixel = reply.pixel
-            if isinstance(reply, xcb.xproto.AllocNamedColorReply):
-                self._red = reply.exact_red
-                self._blue = reply.exact_blue
-                self._green = reply.exact_green
-            else:
-                self._red = reply.red
-                self._green = reply.green
-                self._blue = reply.blue
+            return reply
 
 
     @property
@@ -68,8 +32,6 @@ class Color(XObject):
     @property
     def name(self):
 
-        if hasattr(self, "_name"):
-            return self._name
         return self.hex
 
 
@@ -93,8 +55,98 @@ class Color(XObject):
         self.read_reply()
         return self._green
 
+
     @property
     def blue(self):
 
         self.read_reply()
         return self._blue
+
+    @property
+    def alpha(self):
+
+        return self._alpha
+
+
+    def __str__(self):
+
+        return self.name
+
+
+class NamedColor(Color):
+
+    """A named color."""
+
+    def __init__(self, colormap, name, alpha=65535, **kw):
+
+        Color.__init__(self, **kw)
+
+        if alpha < 0 or alpha > 65535:
+            raise ValueError("Bad alpha value.")
+
+        self.cookie = self.connection.core.AllocNamedColor(colormap, len(name), name)
+        self._name = name
+        self._alpha = alpha
+
+
+    def read_reply(self):
+
+        reply = Color.read_reply(self)
+        if reply:
+            self._red = reply.exact_red
+            self._green = reply.exact_green
+            self._blue = reply.exact_blue
+            return reply
+
+
+    @property
+    def name(self):
+
+        return self._name
+
+
+class ValueColor(Color):
+
+    """A color by value."""
+
+    def __init__(self, colormap, red=0, green=0, blue=0, alpha=65535, **kw):
+
+        Color.__init__(self, **kw)
+
+        for value in [ red, blue, green, alpha ]:
+            if value < 0 or value > 65535:
+                raise ValueError("Color attribute value is too high.")
+
+        self.cookie = self.connection.core.AllocColor(colormap, red, green, blue)
+        self._alpha = alpha
+
+
+    def read_reply(self):
+
+        reply = Color.read_reply(self)
+        if reply:
+            self._red = reply.red
+            self._green = reply.green
+            self._blue = reply.blue
+            return reply
+
+
+class HexColor(ValueColor):
+
+    """An hexadecimal color."""
+
+    def __init__(self, colormap, name, alpha=65535, **kw):
+
+        len_name = len(name)
+
+        if (len_name != 7 and len_name != 9) or name[0] != '#':
+            raise ValueError("Bad color name {0}.".format(name))
+
+        red = int(name[1:3], 16) * 257
+        green = int(name[3:5], 16) * 257
+        blue = int(name[5:7], 16) * 257
+
+        if len_name == 9:
+            alpha = int(name[7:9], 16) * 257
+
+        ValueColor.__init__(self, colormap, red, green, blue, alpha, **kw)
