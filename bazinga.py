760cefd876ae21941f1f654c76da559442d85ea7
Julien Danjou <julien@danjou.info>
2010-01-20 17:49:40 +0100

    Move util in a dir
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- bazinga.py
+++ bazinga.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 if __name__ == "__main__":
-    import bazinga.util as util
+    import bazinga.base as base
     from bazinga.log import log
 
-    util.setup_sys_path()
+    base.setup_sys_path()
 
     try:
-        util.load_config_file()
+        base.load_config_file()
     except Exception, e:
         log.critical("Cannot load configuration file: %s" % e)
