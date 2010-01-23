12f732178a974353b5af034987b6fb3a61a1129c
Julien Danjou <julien@danjou.info>
2010-01-23 12:43:25 +0100

    fix signal
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- bazinga/base/signal.py
+++ bazinga/base/signal.py
@@ -1,14 +1,68 @@
 from louie import *
+from louie.dispatcher import live_receivers, get_receivers, plugins, sends
+
+def get_all_receivers_mro(sender=Any, signal=All):
+    """Get list of all receivers from global tables.
+
+    This gets all receivers which should receive the given signal from
+    sender, each receiver should be produced only once by the
+    resulting generator.
+
+    This also returns receivers matchin the sender and signal MRO.
+    """
+    yielded = set()
+    _receivers = [
+        # Get receivers that receive *this* signal from *this* sender.
+        get_receivers(sender, signal),
+        # Add receivers that receive *all* signals from *this* sender.
+        get_receivers(sender, All),
+        # Add receivers that receive *this* signal from *any* sender.
+        get_receivers(Any, signal),
+        # Add receivers that receive *all* signals from *any* sender.
+        get_receivers(Any, All) ]
+
+    for cls in sender.__class__.__mro__:
+        _receivers.append(get_receivers(cls, signal))
+
+    for cls in signal.__class__.__mro__:
+        _receivers.append(get_receivers(sender, cls))
+
+    for receivers in _receivers:
+        for receiver in receivers:
+            if receiver: # filter out dead instance-method weakrefs
+                try:
+                    if not receiver in yielded:
+                        yielded.add(receiver)
+                        yield receiver
+                except TypeError:
+                    # dead weakrefs raise TypeError on hash...
+                    pass
+
 
 def emit(signal=signal.All, sender=sender.Anonymous, *arguments, **named):
     """Emit a signal. This is the same as send, except that it also emit the
     signal on object classes, following MRO."""
-
-    ret = []
-    for s in [ sender ] + list(sender.__class__.__mro__):
-        for sig in [ signal ] + list(signal.__class__.__mro__):
-            ret += send(sig, s, *arguments, **named)
-    return ret
+    # Call each receiver with whatever arguments it can accept.
+    # Return a list of tuple pairs [(receiver, response), ... ].
+    responses = []
+    for receiver in live_receivers(get_all_receivers_mro(sender, signal)):
+        # Wrap receiver using installed plugins.
+        original = receiver
+        for plugin in plugins:
+            receiver = plugin.wrap_receiver(receiver)
+        response = robustapply.robust_apply(
+            receiver, original,
+            signal=signal,
+            sender=sender,
+            *arguments,
+            **named
+            )
+        responses.append((receiver, response))
+    # Update stats.
+    if __debug__:
+        global sends
+        sends += 1
+    return responses
 
 
 def connected(signal=signal.All, sender=sender.Any, weak=True):
--- tests/TestSignal.py
+++ tests/TestSignal.py
@@ -6,40 +6,49 @@ import bazinga.base.signal as signal
 from bazinga.base.object import Object
 
 class TestSignal(unittest.TestCase):
-
     class Yack(Object):
-
         """A subclass of Object. That's all."""
-
         pass
 
+    def set_changed(self, signal):
+        self.has_changed = True
 
-    def _test_setattr_signal_with_sender(self, obj, sender):
 
-        def f():
-            self.has_changed = True
-
+    def _test_sender(self, sender, sender_to_listen):
         self.has_changed = False
-        signal.connect(f, signal="yo", sender=sender)
-        obj.emit_signal("yo")
+        signal.connect(self.set_changed, signal="yo", sender=sender_to_listen)
+        signal.emit(signal="yo", sender=sender)
         self.assert_(self.has_changed)
-        signal.disconnect(f, signal="yo", sender=sender)
+        signal.disconnect(self.set_changed, signal="yo", sender=sender_to_listen)
 
+    def test_sender_on_class(self):
+        self._test_sender(self.Yack(), self.Yack)
 
-    def test_setattr_signal_on_class(self):
+    def test_sender_on_parent_class(self):
+        self._test_sender(self.Yack(), Object)
 
-        self._test_setattr_signal_with_sender(self.Yack(), self.Yack)
-
-
-    def test_setattr_signal_on_parent_class(self):
-
-        self._test_setattr_signal_with_sender(self.Yack(), Object)
-
-
-    def test_setattr_signal_on_object(self):
-
+    def test_sender_on_object(self):
         y = self.Yack()
-        self._test_setattr_signal_with_sender(y, y)
+        self._test_sender(y, y)
+
+
+    def _test_signal(self, signal_to_emit, signal_to_listen):
+        self.has_changed = False
+        obj = Object()
+        obj.connect_signal(self.set_changed, signal_to_listen)
+        obj.emit_signal(signal_to_emit)
+        self.assert_(self.has_changed)
+        obj.disconnect_signal(self.set_changed, signal_to_listen)
+
+    def test_signal_on_class(self):
+        self._test_signal(self.Yack(), self.Yack)
+
+    def test_signal_on_parent_class(self):
+        self._test_signal(self.Yack(), Object)
+
+    def test_signal_on_object(self):
+        y = self.Yack()
+        self._test_signal(y, y)
 
 
 if __name__ == "__main__":
