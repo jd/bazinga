f67ef00e4d767206c7c9282a5132d8a29eb8c563
Julien Danjou <julien@danjou.info>
2010-01-23 23:48:41 +0100

    remove new signal
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- bazinga/base/object.py
+++ bazinga/base/object.py
@@ -4,12 +4,6 @@ import signal as bsignal
 class Object(object):
     """Base class of many bazinga objects."""
 
-    class New(bsignal.Signal):
-        """Signal emitted on new object creation."""
-
-    def __init__(self, *args, **kw):
-        self.emit_signal(self.New)
-
     def connect_signal(self, receiver, signal=bsignal.signal.All):
         """Connect a signal."""
         return bsignal.connect(receiver, signal=signal, sender=self)
@@ -21,9 +15,3 @@ class Object(object):
     def emit_signal(self, signal=bsignal.signal.All, *args, **kw):
         """Emit a signal on an object."""
         return bsignal.emit(signal, self, *args, **kw)
-
-    @classmethod
-    def on_new(cls, func):
-        """Decorator to connect a function to the New signal."""
-        bsignal.connect(func, signal=cls.New, sender=cls)
-        return func
