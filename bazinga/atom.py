23081ae711841b3865c770e63f13bdf3863e3cc5
Julien Danjou <julien@danjou.info>
2010-01-27 20:14:42 +0100

    update atom
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- bazinga/atom.py
+++ bazinga/atom.py
@@ -1,28 +1,34 @@
-from base.memoize import memoize, Memoized
+from base.memoize import Memoized
+from base.property import cachedproperty
 from base.object import Object
 from x import MainConnection
 
 import xcb.xproto
 
 class XAtom(Object, Memoized):
-    """X Atom."""
+    """X atom."""
 
     def __init__(self, name="Any"):
-        try:
-            self._value = getattr(xcb.xproto.Atom, name)
-        except AttributeError:
-            # No pre-defined atom by that name
-            self._cookie = MainConnection().core.InternAtom(False, len(name), name)
+        self.name = name
 
-    def read_reply(self):
-        if hasattr(self, "_cookie"):
-            self._value = self._cookie.reply().atom
-            del self._cookie
+    class value(cachedproperty):
+        """X atom value."""
 
-    @property
-    def value(self):
-        self.read_reply()
-        return self._value
+        def __get__(self):
+            try:
+                return getattr(xcb.xproto.Atom, self.name)
+            except AttributeError:
+                # No pre-defined atom by that name
+                return MainConnection().core.InternAtom(False,
+                                                        len(self.name),
+                                                        self.name).reply().atom
 
-def Atom(*args, **kwargs):
-    return XAtom(*args, **kwargs).value
+        def __set__(self):
+            raise AttributeError("read-only attribute")
+
+
+def Atom(name):
+    """Get an XAtom value directly.
+    This function is a wrapper to XAtom class.
+    This will return a value you can directly use to feed XCB functions."""
+    return XAtom(name).value
