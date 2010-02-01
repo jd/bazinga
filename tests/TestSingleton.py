11f3305e7fe8876153803edeaf8db84eb6d7e2ce
Julien Danjou <julien@danjou.info>
2010-02-01 20:39:20 +0100

    add singleton pool checks
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- tests/TestSingleton.py
+++ tests/TestSingleton.py
@@ -2,12 +2,16 @@
 
 import unittest
 
-from bazinga.base.singleton import Singleton
+from bazinga.base.singleton import Singleton, SingletonPool
 
 class TestSingleton(unittest.TestCase):
 
     def test_singleton_identity(self):
         self.assert_(Singleton() is Singleton())
+
+    def test_singleton_pool_identify(self):
+        self.assert_(SingletonPool(1, 2) is not SingletonPool(1, 3))
+        self.assert_(SingletonPool(1, 2) is SingletonPool(1, 2))
 
 
 if __name__ == "__main__":
