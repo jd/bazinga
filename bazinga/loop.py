15457c90636ab0897860e2074a453aa9550eaace
Julien Danjou <julien@danjou.info>
2010-02-02 13:37:01 +0100

    syntax
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- bazinga/loop.py
+++ bazinga/loop.py
@@ -6,12 +6,10 @@ from base.object import Object
 
 class Loop(Object, pyev.Loop):
     """An event loop."""
-
     def __init__(self):
         pyev.Loop.__init__(pyev.EVFLAG_NOSIGFD)
 
 
 class MainLoop(Singleton, Loop):
     """Bazinga main loop."""
-    
     pass
